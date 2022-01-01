6a2146c9bd0641c2a2badf158c73384c2fd279c7
NotTimIsReal <alastairrintumathew@gmail.com>
2022-01-01 14:25:45 +1100

    added dockerfiles

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 cd /home/container
 
-# Output Current Java Version
-java -version ## only really needed to show what version is being used. Should be changed for different applications
-
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=`eval $(echo ${STARTUP})`
 
 # Run the Server
 ${MODIFIED_STARTUP}
